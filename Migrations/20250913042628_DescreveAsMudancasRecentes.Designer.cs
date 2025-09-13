8c237e692b1c0ce92a7b8890a0acf10d0bdd54fe
Robert Moraes <robertmoraes13r@gmail.com>
2025-09-13 01:49:40 -0300

    Realiza nova migration e conteineriza a aplicacao

--- Migrations/20250913042628_DescreveAsMudancasRecentes.Designer.cs
+++ Migrations/20250913042628_DescreveAsMudancasRecentes.Designer.cs
@@ -4,6 +4,7 @@ using CliVet.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CliVet.Migrations
 {
     [DbContext(typeof(CliVetContext))]
-    partial class CliVetContextModelSnapshot : ModelSnapshot
+    [Migration("20250913042628_DescreveAsMudancasRecentes")]
+    partial class DescreveAsMudancasRecentes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20250913042628_DescreveAsMudancasRecentes.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CliVet.Migrations
+{
+    /// <inheritdoc />
+    public partial class DescreveAsMudancasRecentes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Cpf",
+                table: "Tutores",
+                type: "nvarchar(11)",
+                maxLength: 11,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(14)",
+                oldMaxLength: 14);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Cpf",
+                table: "Tutores",
+                type: "nvarchar(14)",
+                maxLength: 14,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(11)",
+                oldMaxLength: 11);
+        }
+    }
+}
